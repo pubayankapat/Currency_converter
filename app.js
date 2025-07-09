d805d9b6cdd4f5c58324459cc90dbc27e223c43a
pubayankapat <pubayankapat@gmail.com>
2025-07-10 00:07:15 +0530

    Updating backend

--- app.js
+++ app.js
@@ -4,8 +4,6 @@
 // flag api: https://flagsapi.com/:country_code/:style/:size.png
 
 
-const dropdown = document.querySelectorAll("")
-
 let promise = fetch('https://v6.exchangerate-api.com/v6/07e92174f137e6ad6a59ae0f/latest/USD')
   .then(res => res.json())
   .then(data => {
@@ -13,3 +11,23 @@ let promise = fetch('https://v6.exchangerate-api.com/v6/07e92174f137e6ad6a59ae0f
   });
 
 // console.log(promise);
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+
+for (let select of dropdowns) {
+  for (code in countryList) {
+    let option = document.createElement("option");
+    option.innerText = code;
+    option.value = code;
+    if(select.name === "from" && code === "USD"){
+      option.selected = "selected";
+    }else if(select.name === "to" && code === "INR"){
+        option.selected = "selected";
+    }
+    select.append(option);
+  }
+}
+
+const updateFlag = () => {
+
+}
