4927221cc34b3de6984b131422e149108905d972
pubayankapat <pubayankapat@gmail.com>
2025-07-09 12:32:43 +0530

    change in app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-BASE_URL = ;
+// exchangerate-api
+// api key: 07e92174f137e6ad6a59ae0f
+
+// flag api: https://flagsapi.com/:country_code/:style/:size.png
+
+fetch(' https://v6.exchangerate-api.com/v6/07e92174f137e6ad6a59ae0f/latest/USD')
+  .then(res => res.json())
+  .then(data => {
+    console.log(`1 USD = ${data.conversion_rates.INR} INR`);
+  });
